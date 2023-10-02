68316f4878686d776e26d7beebe859cc52aa5141
Etanami Olatunji <etanamiolatunji@gmail.com>
2023-10-02 15:58:11 +0100

    add control and post request functionality

--- content.js
+++ content.js
@@ -1,39 +1,36 @@
-var recorder = null;
 
-const onAccessApproved = (stream) => {
-  recorder = new MediaRecorder(stream);
+// start button
+const startButton = document.getElementById('#start_recording')
 
-  recorder.start();
+// Create the Stop button
+const stopButton = document.createElement('button');
+stopButton.id = 'stop-button';
+stopButton.textContent = 'Stop';
+stopButton.disabled = true;
 
-  recorder.onstop = () => {
-    stream.getTracks().forEach(track => {
-        if(track.readyState === "live"){
-          track.stop()
-        }
-    })
-  }
+// Create the Pause button
+const pauseButton = document.createElement('button');
+pauseButton.id = 'pause-button';
+pauseButton.textContent = 'Pause';
+pauseButton.disabled = true;
 
-  recorder.ondataavailable = event => {
-      let recordedBlob  = event.data;
-      let url = URL.createObjectURL(recordedBlob);
+// Create the Resume button
+const resumeButton = document.createElement('button');
+resumeButton.id = 'resume-button';
+resumeButton.textContent = 'Resume';
+resumeButton.disabled = true;
 
-      console.log(event)
-      console.log(url)
+// Append the buttons to the document body or any other container element
+document.body.appendChild(stopButton);
+document.body.appendChild(pauseButton);
+document.body.appendChild(resumeButton);
 
-      let a = document.createElement("a");
-
-      a.style.display = "none";
-      a.href = url;
-      a.download = "screen-recording.webm"
-
-      document.body.appendChild(a);
-      a.click();
-
-      document.body.removeChild(a);
-
-      URL.revokeObjectURL(url);
-  }
-}
+// Declare global variables
+let recorder;
+let isRecording = false;
+let isPaused = false;
+let recordedChunks = [];
+let videoId;
 
 chrome.runtime.onMessage.addListener( (message, sender, sendResponse)=>{
 
@@ -48,8 +45,119 @@ chrome.runtime.onMessage.addListener( (message, sender, sendResponse)=>{
           width:9999999999,
           height: 9999999999
       }
-    }).then((stream)=>{
-        onAccessApproved(stream)
-    })  
+    }).then((stream) => {
+      recorder = new MediaRecorder(stream);
+      recordedChunks = [];
+
+      // Set the desired time slice (chunk size) in milliseconds
+      const timeSlice = 5000; // 5 seconds
+
+      recorder.ondataavailable = (event) => {
+          if (event.data.size > 0) {
+              recordedChunks.push(event.data);
+              console.log('recorded chunks', recordedChunks)
+          }
+      };
+
+      recorder.onstop = () => {
+          const blob = new Blob(recordedChunks, { type: "video/webm" });
+
+          // Send the blob to your backend
+          sendBlobToBackend(blob);
+
+          // Clean up
+          recordedChunks = [];
+          stream.getTracks().forEach((track) => {
+            if(track.readyState === "live"){
+              track.stop()
+            }
+          });
+        };
+
+        recorder.start(timeSlice);
+        isRecording = true;
+
+        // Update UI
+        stopButton.disabled = false;
+        pauseButton.disabled = false;
+        resumeButton.disabled = true;
+    })
+    .catch((error) => {
+        console.error("Error starting recording:", error);
+    }); 
   }
 })
+
+// Function to send the blob data to the backend
+function sendBlobToBackend(blob) {
+  const formData = new FormData();
+  formData.append("video", blob, "screen-recording.webm");
+
+  // from BE dev
+  let url = "https://helpmeout-ext.onrender.com";
+
+//   let get = fetch(url)
+//   console.log(get)
+
+  fetch(url, {
+      method: "POST",
+      body: formData,
+  })
+    .then((response) => {
+        if (response.ok) {
+            console.log("Recording data sent successfully.");
+        } else {
+            console.error("Failed to send video to the backend", response.status);
+        }
+    })
+    .catch((error) => {
+        console.error("Error sending blob data:", error);
+    });
+}
+
+
+// Function to stop recording
+function stopRecording() {
+    if (isRecording) {
+        // Stop recording logic
+        recorder.stop();
+
+        // Update UI
+        // startButton.disabled = false;
+        stopButton.disabled = true;
+        pauseButton.disabled = true;
+        resumeButton.disabled = true;
+    }
+}
+
+// Function to pause recording
+function pauseRecording() {
+    if (isRecording && !isPaused) {
+        // Pause recording logic
+        recorder.pause();
+        isPaused = true;
+
+        // Update UI
+        pauseButton.disabled = true;
+        resumeButton.disabled = false;
+    }
+}
+
+// Function to resume recording
+function resumeRecording() {
+    if (isRecording && isPaused) {
+        // Resume recording logic
+        recorder.resume();
+        isPaused = false;
+
+        // Update UI
+        pauseButton.disabled = false;
+        resumeButton.disabled = true;
+    }
+}
+
+
+// Add event listeners to the buttons
+stopButton.addEventListener("click", stopRecording);
+pauseButton.addEventListener("click", pauseRecording);
+resumeButton.addEventListener("click", resumeRecording);
